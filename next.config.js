24357eaef94c10b460a57a8a7cf44fa06d40f91e
r800zz <admin@vr180g.com>
2025-09-13 08:29:55 +0700

    Update next.config.js
    Added configuration to support "yarn export", enabling conversion from index.tsx to static index.html.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Export as a static site
+  basePath: '/bvh2vrma-7lang', // Match the path for GitHub Pages
+  assetPrefix: '/bvh2vrma-7lang', // Adjust paths for loading CSS and JS
+
   webpack(config) {
     config.module.rules.push({
       test: /\.vrm$/u,
@@ -8,7 +12,8 @@ const nextConfig = {
     })
     return config
   },
-  compiler:{
+
+  compiler: {
     removeConsole: process.env.NODE_ENV === 'production'
   }
 }
